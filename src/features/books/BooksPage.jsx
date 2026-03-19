babe916be690afde1ae5cc8a1493bd678d1a34b3
varshaXcodes <v81760632@gmail.com>
2026-03-19 19:50:56 +0530

    work complete

--- src/features/books/BooksPage.jsx
+++ src/features/books/BooksPage.jsx
@@ -1,5 +1,23 @@
-export function BooksPage(
-) 
-{ 
-    return <div><h2>Books</h2><p>Books management page. hey I'm varsha.</p></div>;
+import Table from "../../components/table/Table";
+
+import "./Bookspage.css";
+
+
+export function BooksPage() {
+    
+    return <>
+    
+    <div>
+        
+<div class="parent">
+    <div class="div1"><h2>Books</h2></div>
+    <div class="div2"><p>Manage catalog metadata and book availability.</p></div>
+    <div class="div3"><input></input></div>
+    <div class="div4"><button> + Book</button></div>
+</div>
+
+ <Table/>
+    
+        </div></>
+
 }
