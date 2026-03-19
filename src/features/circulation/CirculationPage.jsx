fdd6ce810c4f00700e47bf7c9fd584a6b094b903
Samyakjmauar <samyakjamuar.sj@gmail.com>
2026-03-19 07:42:06 +0530

    Table File

--- src/features/circulation/CirculationPage.jsx
+++ src/features/circulation/CirculationPage.jsx
@@ -1,3 +1,9 @@
-export function CirculationPage() { return <div><h2>Circulation</h2>
+import Table from "../../components/table/Table";
+
+  
+  
+  
+  export function CirculationPage() { return <div><h2>Circulation</h2>
 <div>hi i am made by samyak</div>
+<Table/>
 <p>Circulation page.</p></div>; }
